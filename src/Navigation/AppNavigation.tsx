cab39e70bbe4e66a9054e29c604357127b9bab89
sarveshwar.sivakumar <sarveshwar.sivakumar.contractor@pepsico.com>
2024-10-24 15:14:08 +0530

    UI-changes

--- src/Navigation/AppNavigation.tsx
+++ src/Navigation/AppNavigation.tsx
@@ -4,6 +4,7 @@ import UserScreen1 from '../Screens/UserScreen1';
 import UserScreen2 from '../Screens/UserScreen2';
 import UserScreen3 from '../Screens/UserScreen3';
 import UserScreen4 from '../Screens/UserScreen4';
+import UserScreen5 from '../Screens/UserScreen5';
 
 const AppNavigator = () => {
   const stack = createNativeStackNavigator();
@@ -14,6 +15,7 @@ const AppNavigator = () => {
         <stack.Screen name="UserScreen2" component={UserScreen2} />
         <stack.Screen name="UserScreen3" component={UserScreen3} />
         <stack.Screen name="UserScreen4" component={UserScreen4} />
+        <stack.Screen name="UserScreen5" component={UserScreen5} />
       </stack.Navigator>
     </NavigationContainer>
   );
