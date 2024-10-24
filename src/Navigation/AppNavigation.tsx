a397efcd2c46dccdf657525e6bd08baa443c2dae
sarveshwar.sivakumar <sarveshwar.sivakumar.contractor@pepsico.com>
2024-10-24 14:43:39 +0530

    New-commit

--- src/Navigation/AppNavigation.tsx
+++ src/Navigation/AppNavigation.tsx
@@ -1,6 +1,7 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import UserScreen1 from '../Screens/UserScreen1';
+import UserScreen2 from '../Screens/UserScreen2';
 
 const AppNavigator = () => {
   const stack = createNativeStackNavigator();
@@ -8,6 +9,7 @@ const AppNavigator = () => {
     <NavigationContainer>
       <stack.Navigator initialRouteName="UserScreen1">
         <stack.Screen name="UserScreen1" component={UserScreen1} />
+        <stack.Screen name="UserScreen2" component={UserScreen2} />
       </stack.Navigator>
     </NavigationContainer>
   );
