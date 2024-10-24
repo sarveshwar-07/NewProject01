80242eb16c7b16678d3cd542b497d3876c8bdc5f
sarveshwar.sivakumar <sarveshwar.sivakumar.contractor@pepsico.com>
2024-10-24 14:23:03 +0530

    first commit01

--- src/Navigation/AppNavigation.tsx
+++ src/Navigation/AppNavigation.tsx
@@ -1,11 +1,15 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
+import UserScreen1 from '../Screens/UserScreen1';
 
 const AppNavigator = () => {
   const stack = createNativeStackNavigator();
   return (
-    
+    <NavigationContainer>
+      <stack.Navigator initialRouteName="UserScreen1">
+        <stack.Screen name="UserScreen1" component={UserScreen1} />
+      </stack.Navigator>
+    </NavigationContainer>
   );
 };
 
