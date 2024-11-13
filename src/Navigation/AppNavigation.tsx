351d5ae02da3de8b7cbf4b9d891238bfad149794
sarveshwar.sivakumar <sarveshwar.sivakumar.contractor@pepsico.com>
2024-11-13 13:47:54 +0530

    NewScreen

--- src/Navigation/AppNavigation.tsx
+++ src/Navigation/AppNavigation.tsx
@@ -2,20 +2,14 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import UserScreen1 from '../Screens/UserScreen1';
 import UserScreen2 from '../Screens/UserScreen2';
-import UserScreen3 from '../Screens/UserScreen3';
-import UserScreen4 from '../Screens/UserScreen4';
-import UserScreen5 from '../Screens/UserScreen5';
 
 const AppNavigator = () => {
   const stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <stack.Navigator initialRouteName="UserScreen1">
+      <stack.Navigator initialRouteName="UserScreen2">
         <stack.Screen name="UserScreen1" component={UserScreen1} />
         <stack.Screen name="UserScreen2" component={UserScreen2} />
-        <stack.Screen name="UserScreen3" component={UserScreen3} />
-        <stack.Screen name="UserScreen4" component={UserScreen4} />
-        <stack.Screen name="UserScreen5" component={UserScreen5} />
       </stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/Screens/UserScreen2/index.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+
+const UserScreen2 = () => {
+  return (
+    <View>
+      <Text>Sarvesh02</Text>
+    </View>
+  );
+};
+
+export default UserScreen2;
