23d76819a98702354d5e3112908a070c2037012a
sarveshwar.sivakumar <sarveshwar.sivakumar.contractor@pepsico.com>
2024-11-13 14:00:51 +0530

    NewScreen

--- src/Navigation/AppNavigation.tsx
+++ src/Navigation/AppNavigation.tsx
@@ -2,14 +2,16 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import UserScreen1 from '../Screens/UserScreen1';
 import UserScreen2 from '../Screens/UserScreen2';
+import UserScreen3 from '../Screens/UserScreen3';
 
 const AppNavigator = () => {
   const stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <stack.Navigator initialRouteName="UserScreen2">
+      <stack.Navigator initialRouteName="UserScreen3">
         <stack.Screen name="UserScreen1" component={UserScreen1} />
         <stack.Screen name="UserScreen2" component={UserScreen2} />
+        <stack.Screen name={'UserScreen3'} component={UserScreen3} />
       </stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/Screens/UserScreen3/index.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+
+const UserScreen3 = () => {
+  return (
+    <View>
+      <Text>Sarvesh03</Text>
+    </View>
+  );
+};
+
+export default UserScreen3;
